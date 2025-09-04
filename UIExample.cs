7bae285a29a5d881db6f959d67532888e690ad43
GT1650CS <hotlap8878@gmail.com>
2025-09-04 19:17:25 +0100

    Update UIExample.cs - edited window positioning

--- UIExample.cs
+++ UIExample.cs
@@ -13,7 +13,7 @@ namespace SFS_ExampleMod
         {
             uiHolder = Builder.CreateHolder(Builder.SceneToAttach.CurrentScene, "ExampleMod GUI Container");
 
-            Window window = UIToolsBuilder.CreateClosableWindow(uiHolder.transform, windowID, 300, 180, 200, 200, true, true, 0.85f, "ExampleMod", false);
+            Window window = UIToolsBuilder.CreateClosableWindow(uiHolder.transform, windowID, 300, 180, -1280, 720, true, true, 0.85f, "ExampleMod", false);
             window.CreateLayoutGroup(Type.Vertical);
 
             Container windowContainer = Builder.CreateContainer(window);
